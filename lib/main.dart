5af64e575dc801ebeeaadcab4a75d113d1721352
Dariel Subiros <dsubiros@gmail.com>
2024-12-30 01:00:07 -0500

    - Can show todo. - Request focus on item tap. If item is focused show using TextField, otherwise show using Text

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_todo_2/provider.dart';
+import 'package:riverpod_todo_2/todo.dart';
 
 void main() => runApp(const ProviderScope(child: MyApp()));
 
@@ -79,9 +80,10 @@ class Home extends HookConsumerWidget {
                 key: ValueKey(todo.id),
                 onDismissed: (direction) =>
                     ref.read(todoListProvider.notifier).remove(todo),
-                child: ListTile(
-                  title: Text(todo.description),
-                ),
+                // child: ListTile(title: Text(todo.description)),
+                child: ProviderScope(
+                    overrides: [_currentTodo.overrideWithValue(todo)],
+                    child: const TodoItem()),
               ),
             ),
           ],
@@ -89,6 +91,71 @@ class Home extends HookConsumerWidget {
       ),
     );
   }
+}
+
+/// A provider which exposes the [Todo] displayed by a [TodoItem].
+///
+/// By retrieving the [Todo] through a provider instead of through its
+/// constructor, this allows [TodoItem] to be instantiated using the `const` keyword.
+///
+/// This ensures that when we add/remove/edit todos, only what the
+/// impacted widgets rebuilds, instead of the entire list of items.
+final _currentTodo = Provider<Todo>((ref) => throw UnimplementedError());
+
+class TodoItem extends HookConsumerWidget {
+  const TodoItem({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todo = ref.watch(_currentTodo);
+    final itemFocusNode = useFocusNode();
+    final itemIsFocused = useIsFocused(itemFocusNode);
+
+    final textEditingController = useTextEditingController();
+    final textFieldFocusNode = useFocusNode();
+
+    return Material(
+        color: Colors.white,
+        elevation: 6,
+        child: Focus(
+            focusNode: itemFocusNode,
+            onFocusChange: (focus) {
+              if (focus) {
+                textEditingController.text = todo.description;
+              } else {
+                // Commit changes only when the textfield is unfocused, for performance
+                ref
+                    .read(todoListProvider.notifier)
+                    .edit(id: todo.id, description: textEditingController.text);
+              }
+            },
+            child: ListTile(
+                onTap: () {
+                  itemFocusNode.requestFocus();
+                  textFieldFocusNode.requestFocus();
+                },
+                title: itemIsFocused
+                    ? TextField(
+                        autofocus: true,
+                        focusNode: textFieldFocusNode,
+                        controller: textEditingController,
+                      )
+                    : Text(todo.description))));
+  }
+}
+
+bool useIsFocused(FocusNode node) {
+  final isFocused = useState(node.hasFocus);
+
+  useEffect(() {
+    void listener() {
+      isFocused.value = node.hasFocus;
+    }
+
+    node.addListener(listener);
+    return () => node.removeListener(listener);
+  }, [node]);
+
+  return isFocused.value;
 }
 
 class Tile extends StatelessWidget {
