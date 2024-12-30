f90b58674bedf6bca34833504841ba13da736436
Dariel Subiros <dsubiros@gmail.com>
2024-12-29 22:32:53 -0500

    Show the Todos on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_todo_2/old/main.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+
     return MaterialApp(
-      title: 'Material App',
+      // title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+        // appBar: AppBar(
+        //   title: const Text('Material App Bar'),
+        // ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Text(
+                'What needs to be done?',
+                style: textTheme.displaySmall,
+              ),
+              const TodoPage()
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class TodoPage extends ConsumerWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final data = ref.watch(todoListProvider);
+
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: data.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(data[index].description),
+        );
+      },
     );
   }
 }
