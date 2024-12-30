75cee72b5f56cebafb99ca9e67ed1f6749e76332
Dariel Subiros <dsubiros@gmail.com>
2024-12-30 00:19:36 -0500

    Can already add new Todo, using textEditing Controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_todo_2/provider.dart';
 
 void main() => runApp(const ProviderScope(child: MyApp()));
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var textTheme = Theme.of(context).textTheme;
+
+//     return MaterialApp(
+//       // title: 'Material App',
+//       home: Scaffold(
+//         // appBar: AppBar(
+//         //   title: const Text('Material App Bar'),
+//         // ),
+//         body: SafeArea(
+//           child: Column(
+//             children: [
+//               Text(
+//                 'What needs to be done?',
+//                 style: textTheme.displaySmall,
+//               ),
+//               const Home()
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,43 +41,80 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
 
-    return MaterialApp(
-      // title: 'Material App',
-      home: Scaffold(
-        // appBar: AppBar(
-        //   title: const Text('Material App Bar'),
-        // ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Text(
-                'What needs to be done?',
-                style: textTheme.displaySmall,
-              ),
-              const TodoPage()
-            ],
-          ),
+    return const MaterialApp(
+      title: 'Riverpod Todos',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends HookConsumerWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todos = ref.watch(filteredTodos);
+    final newTodoController = useTextEditingController();
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          children: [
+            const Tile(),
+            TextField(
+              key: addTodoKey,
+              controller: newTodoController,
+              decoration:
+                  const InputDecoration(labelText: 'What needs to be done?'),
+              onSubmitted: (value) {
+                ref.read(todoListProvider.notifier).add(value);
+                newTodoController.clear();
+              },
+            ),
+            const SizedBox(height: 42.0),
+            ...todos.map((todo) => ListTile(title: Text(todo.description))),
+          ],
         ),
       ),
     );
   }
 }
 
-class TodoPage extends HookConsumerWidget {
-  const TodoPage({super.key});
+class Tile extends StatelessWidget {
+  const Tile({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final data = ref.watch(filteredTodos);
-
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: data.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(data[index].description),
-        );
-      },
+  Widget build(BuildContext context) {
+    return const Text(
+      'todos',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Colors.orange,
+        fontSize: 100,
+        fontWeight: FontWeight.w100,
+        fontFamily: 'Helvetica Neue',
+      ),
     );
   }
 }
+
+// class TodoPage extends HookConsumerWidget {
+//   const TodoPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final data = ref.watch(filteredTodos);
+
+//     return ListView.builder(
+//       shrinkWrap: true,
+//       itemCount: data.length,
+//       itemBuilder: (context, index) {
+//         return ListTile(
+//           title: Text(data[index].description),
+//         );
+//       },
+//     );
+//   }
+// }
